5d981538cff4dc4fd1491f8b37eec4b18de654c6
apl71 <luyingjie1998@outlook.com>
2023-01-16 16:45:40 +0800

    去除冗余代码

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,7 +3,6 @@
 int main() {
     // clear screen and print banner
     clear_screen();
-    int a[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-    char message[] = "Welcome to OS.\0";
-    print_string_at(message, 0, 0, GREEN_ON_BLACK);
+    char message[] = "Welcome to LOS.\n";
+    print_string(message, GREEN_ON_BLACK);
 }
