c759e4505991b5fa6d0c2b8009cda294e03dd3bc
apl71 <luyingjie1998@outlook.com>
2023-01-16 11:10:08 +0800

    print chars

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,24 @@
 #include "../drivers/screen.h"
 
-void main() {
-    // 清除屏幕，然后打印欢迎语句
+int main() {
+    // clear screen and print banner
     clear_screen();
-    char message[] = "Welcome to LOS.";
-    print_string_at(message, 0, 0, GREEN_ON_BLACK);
+    print_char_at('W',  0, 0, GREEN_ON_BLACK);
+    print_char_at('e',  1, 0, GREEN_ON_BLACK);
+    print_char_at('l',  2, 0, GREEN_ON_BLACK);
+    print_char_at('c',  3, 0, GREEN_ON_BLACK);
+    print_char_at('o',  4, 0, GREEN_ON_BLACK);
+    print_char_at('m',  5, 0, GREEN_ON_BLACK);
+    print_char_at('e',  6, 0, GREEN_ON_BLACK);
+    print_char_at(' ',  7, 0, GREEN_ON_BLACK);
+    print_char_at('t',  8, 0, GREEN_ON_BLACK);
+    print_char_at('o',  9, 0, GREEN_ON_BLACK);
+    print_char_at(' ', 10, 0, GREEN_ON_BLACK);
+    print_char_at('O', 11, 0, GREEN_ON_BLACK);
+    print_char_at('S', 12, 0, GREEN_ON_BLACK);
+    print_char_at('.', 13, 0, GREEN_ON_BLACK);
+    print_char_at('\0',14, 0, GREEN_ON_BLACK);
+    int a[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    char message[] = "Welcome to OS.\0";
+    // print_string_at(message, 0, 0, GREEN_ON_BLACK);
 }
