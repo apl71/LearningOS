e4451a19fd228d4b2512040c4ee3eac37adfca59
apl71 <luyingjie1998@outlook.com>
2023-01-16 16:45:02 +0800

    新增光标移动、屏幕滚动、光标处打印字符串功能

--- drivers/screen.c
+++ drivers/screen.c
@@ -1,29 +1,56 @@
 #include "screen.h"
 #include "../kernel/low_level.h"
+#include "../kernel/util.h"
 
 void print_char_at(char character, int col, int row, char attr) {
     // 获取显示设备映射主存的首地址
     unsigned char *vidmem = VIDEO_ADDRESS;
-    // 计算坐标偏移量
-    int offset = get_screen_offset(col, row);
     // 默认属性是白底黑字
     if (!attr) {
         attr = WHITE_ON_BLACK;
     }
-    // 填充数据
-    vidmem[offset++] = character;
-    vidmem[offset++] = attr;
+    int offset = 0;
+    if (col < 0 || row < 0) {
+        // 获取光标位置
+        offset = get_cursor();
+    } else {
+        // 计算内存偏移量
+        offset = get_screen_offset(col, row);
+    }
+    // 处理回车
+    if (character == '\n') {
+        int rows = offset / (2 * MAX_COLS);
+        offset = get_screen_offset(0, rows + 1);
+    } else {
+        // 填充数据
+        vidmem[offset++] = character;
+        vidmem[offset++] = attr;
+    }
+    // 处理滚动屏幕
+    offset = handle_scrolling(offset);
+    // 设定光标位置
+    set_cursor(offset);
 }
 
 void print_string_at(char message[], int col, int row, char attr) {
+    if (col < 0 || row < 0) {
+        int offset = get_cursor();
+        row = offset / (2 * MAX_COLS);
+        col = (offset / 2) % MAX_COLS;
+    }
     int count = 0;
     while (message[count] != 0x00) {
-        print_char_at(message[count++], col++, row, attr);
-        if (col >= MAX_COLS) {
+        print_char_at(message[count], col++, row, attr);
+        if (col >= MAX_COLS || message[count] == '\n') {
             col = 0;
             row++;
         }
+        count++;
     }
+}
+
+void print_string(char message[], char attr) {
+    print_string_at(message, -1, -1, attr);
 }
 
 int get_screen_offset(int col, int row) {
@@ -56,4 +83,27 @@ void clear_screen() {
         }
     }
     set_cursor(get_screen_offset(0, 0));
+}
+
+int handle_scrolling(int cursor_offset) {
+    // 没有超出屏幕，无需处理
+    if (cursor_offset < MAX_COLS * MAX_ROWS * 2) {
+        return cursor_offset;
+    }
+    // 每行后退一行
+    int row;
+    for (row = 1; row < MAX_ROWS; ++row) {
+        memory_copy(get_screen_offset(0, row) + VIDEO_ADDRESS,
+                    get_screen_offset(0, row - 1) + VIDEO_ADDRESS,
+                    2 * MAX_COLS);
+    }
+    // 最后一行清零
+    char *last_line = get_screen_offset(0, MAX_ROWS - 1) + VIDEO_ADDRESS;
+    int i;
+    for (i = 0; i < MAX_COLS * 2; ++i) {
+        last_line[i] = 0;
+    }
+    // 偏移回退一行
+    cursor_offset = get_screen_offset(0, MAX_ROWS - 1);
+    return cursor_offset;
 }
