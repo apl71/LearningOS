ddcc314f8451b1d95dd43f238682a2ac4cbd8537
apl71 <luyingjie1998@outlook.com>
2023-02-01 16:47:56 +0800

    新增测试中断代码和内核无限循环

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,12 +1,21 @@
 #include "../drivers/screen.h"
 #include "interrupt.h"
+#include "pic.h"
 
 int main() {
     // clear screen and print banner
     clear_screen();
-    char hello[] = "Welcome to LOS.\n";
+    char hello[] = "Welcome to my OS.\n";
     print_string(hello, GREEN_ON_BLACK);
-    char int_message[] = "Initializing interrupt.\n";
-    print_string(int_message, GREEN_ON_BLACK);
+    char idt_message[] = "Initializing IDT.\n";
+    print_string(idt_message, GREEN_ON_BLACK);
+    // 初始化IDT
     idt_init();
+
+    // 测试中断
+    __asm__ volatile ("int $0x21");
+
+    for(;;) {
+        asm("hlt");
+    }
 }
