f80170e8bed518af210256c232fd5e663578a673
apl71 <luyingjie1998@outlook.com>
2023-02-02 16:25:35 +0800

    常量字符串更改为define定义

--- kernel/const.h
+++ kernel/const.h
@@ -1,15 +1,26 @@
 #ifndef CONST_H_
 #define CONST_H_
 
-static char ok_msg[] = " [Done]\n";
+// static char ok_msg[] = " [Done]\n";
 
-static char os_hello_msg[] = "Welcome to my OS.\n";
-static char idt_init_msg[] = "Initializing IDT.\n";
-static char pic_init_msg[] = "Initializing PIC.\n";
-static char exception_msg[]  = "Loading ISRs for exceptions.";
-static char set_idt_33_msg[] = "Loading ISR for interrupt vector 33.";
-static char load_idtr_msg[]  = "Loading IDTR and enabling hardware interrupt.";
-static char pic_remap_msg[]  = "Remapping pic.";
-static char clear_mask_msg[] = "Clearing mask for IRQ 1.";
+// static char os_hello_msg[] = "Welcome to my OS.\n";
+// static char idt_init_msg[] = "Initializing IDT.\n";
+// static char pic_init_msg[] = "Initializing PIC.\n";
+// static char exception_msg[]  = "Loading ISRs for exceptions.";
+// static char set_idt_33_msg[] = "Loading ISR for interrupt vector 33.";
+// static char load_idtr_msg[]  = "Loading IDTR and enabling hardware interrupt.";
+// static char pic_remap_msg[]  = "Remapping pic.";
+// static char clear_mask_msg[] = "Clearing mask for IRQ 1.";
+
+#define ok_msg          "[Done]\n"
+
+#define os_hello_msg    "Welcome to my OS.\n"
+#define idt_init_msg    "Initializing IDT.\n"
+#define pic_init_msg    "Initializing PIC.\n"
+#define exception_msg   "Loading ISRs for exceptions. "
+#define set_idt_33_msg  "Loading ISR for interrupt vector 33. "
+#define load_idtr_msg   "Loading IDTR and enabling hardware interrupt. "
+#define pic_remap_msg   "Remapping pic. "
+#define clear_mask_msg  "Clearing mask for IRQ 1. "
 
 #endif
