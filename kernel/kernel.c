5994e70051c5a3d371300cd0337161809937b658
apl71 <luyingjie1998@outlook.com>
2023-02-02 14:51:16 +0800

    去除测试，先初始化PIC，后初始化IDT

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,15 +7,17 @@ int kernel_main() {
     // clear screen and print banner
     clear_screen();
     print_string(os_hello_msg, WHITE_ON_BLACK);
-    // 初始化IDT
-    print_string(idt_init_msg, WHITE_ON_BLACK);
-    idt_init();
     // 初始化PIC
     print_string(pic_init_msg, WHITE_ON_BLACK);
     pic_init();
-
+    // 初始化IDT
+    print_string(idt_init_msg, WHITE_ON_BLACK);
+    idt_init();
+    
     // 测试中断
-    __asm__ volatile ("int $0x21");
+    // __asm__ volatile ("int $0x21");
+    // __asm__ volatile ("int $0x21");
+    // __asm__ volatile ("int $0x21");
 
     for(;;) {
         asm("hlt");
