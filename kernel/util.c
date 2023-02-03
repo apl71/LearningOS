0f037ba1c7b6363a30e37628693b58b134d781c7
apl71 <luyingjie1998@outlook.com>
2023-02-03 15:56:20 +0800

    新增主存设置函数和一些辅助函数

--- kernel/util.c
+++ kernel/util.c
@@ -1,8 +1,25 @@
 #include "util.h"
 
 void memory_copy(char *source, char *dest, int nbyte) {
-    int i;
-    for (i = 0; i < nbyte; ++i) {
+    for (int i = 0; i < nbyte; ++i) {
         dest[i] = source[i];
     }
+}
+
+void memory_set(char *mem, char value, int length) {
+    for (int i = 0; i < length; ++i) {
+        mem[i] = value;
+    }
+}
+
+int len_nums(int32_t num, int radix) {
+    if (num == 0) {
+        return 1;
+    }
+    int count = 0;
+    while (num != 0) {
+        count++;
+        num /= radix;
+    }
+    return count;
 }
--- kernel/util.h
+++ kernel/util.h
@@ -1,6 +1,13 @@
 #ifndef UTIL_H_
 #define UTIL_H_
 
+#include <stdint.h>
+
 void memory_copy(char *source, char *dest, int nbyte);
+
+void memory_set(char *mem, char value, int length);
+
+// 计算数字位数
+int len_nums(int32_t num, int radix);
 
 #endif
