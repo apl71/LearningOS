6605da337738eaa95750185c7fcf5b56432e6267
apl71 <luyingjie1998@outlook.com>
2023-02-03 16:55:54 +0800

    探测低内存

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,6 +2,7 @@
 #include "interrupt.h"
 #include "pic.h"
 #include "const.h"
+#include "memory.h"
 
 int kernel_main() {
     // clear screen and print banner
@@ -13,6 +14,12 @@ int kernel_main() {
     // 初始化IDT
     print_string(idt_init_msg, WHITE_ON_BLACK);
     idt_init();
+    // 探测主存
+    print_string(detect_mem_msg, WHITE_ON_BLACK);
+    print_string(low_mem_msg, WHITE_ON_BLACK);
+    uint32_t low_memory = get_low_memory();
+    print_int(low_memory, 10, WHITE_ON_BLACK);
+    print_string(new_line, WHITE_ON_BLACK);
 
     for(;;) {
         asm("hlt");
