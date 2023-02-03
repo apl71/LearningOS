7580dab8450bc0c5177eea1c847263121e429042
apl71 <luyingjie1998@outlook.com>
2023-02-03 16:33:42 +0800

    完善注释

--- kernel/util.h
+++ kernel/util.h
@@ -7,7 +7,7 @@ void memory_copy(char *source, char *dest, int nbyte);
 
 void memory_set(char *mem, char value, int length);
 
-// 计算数字位数
+// 计算数字位数，radix表示进制，结果不计算符号占位，仅计算数码占位
 int len_nums(int32_t num, int radix);
 
 #endif
