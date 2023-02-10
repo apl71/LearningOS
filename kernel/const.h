ffa2fa94ce1ee44e0f6f777ac72c2be55f8be67a
apl71 <luyingjie1998@outlook.com>
2023-02-10 13:35:21 +0800

    新增一些常量字符串

--- kernel/const.h
+++ kernel/const.h
@@ -2,6 +2,8 @@
 #define CONST_H_
 
 #define new_line            "\n"
+#define blank               " "
+#define hex_head            "0x"
 
 #define ok_msg              "[Done]\n"
 
@@ -14,5 +16,13 @@
 #define pic_remap_msg       "Remapping pic. "
 #define clear_mask_msg      "Clearing mask for IRQ 1. "
 #define mmap_length_msg     "MMAP length = "
+#define mmap_title_msg      "Base Address       Length             Type                          ACPI 3.x\n"
+#define usable_mem_msg      "1: Usable memory              "
+#define reserved_mem_msg    "2: Reserved memory            "
+#define acpi_reclaim_msg    "3: ACPI reclaimable memory    "
+#define acpi_nvs_msg        "4: ACPI NVS memory            "
+#define bad_mem_msg         "5: Area containing bad memory " 
+#define unknown_mem_msg     "Unknown memory type                "
+#define mem_size_msg        "Usable memory size in bytes: "
 
 #endif
