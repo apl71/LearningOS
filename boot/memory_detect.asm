bdb03dc3c17e674f59bc4ac617d7a5ef6b6dd2d3
apl71 <luyingjie1998@outlook.com>
2023-02-10 16:14:58 +0800

    强制要求acpi项

--- boot/memory_detect.asm
+++ boot/memory_detect.asm
@@ -21,7 +21,7 @@ init_mmap:
 mmap_loop:
     mov eax, 0xe820             ; magic number
     mov ecx, 24                 ; 请求长为24字节的项
-;    mov [es:di + 20], dword 1   ; 强制要求apci项
+    mov [es:di + 20], dword 1   ; 强制要求acpi项
     int 0x15
     cmp eax, 0x534d4150         ; eax不为0x534d4150时报错
     jne fail_init_mmap
